71856dc8828e4be75389a082bd54d08f98e00735
Gulyaev Stepan <nlm@yandex.ru>
2020-09-11 21:19:21 +0300

    5 Lesson

--- src/ru/geekbrains/algorithms/Main.java
+++ src/ru/geekbrains/algorithms/Main.java
@@ -6,182 +6,67 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		double time;
-		
-		LinkedList<ArrObj> objList = new LinkedList<>();
-        objList.add(new ArrObj(1, 1));
-        objList.add(new ArrObj(2, 2));
-        objList.add(new ArrObj(3, 3));
+        /*
+        * Пример рекурсии из жизни.
+        * 2 зеркала, стоящих друг напротив друга.
+        * Каждое из них будет бесконечно отражать само себя
+        * с помощью противоположного зеркала.
+        * */
+
+        double time;
+
+        System.out.println(recursion3(1));
 
         time = System.nanoTime();
-		StackM<ArrObj> objStack = new StackM<>(objList);
-        for (int i = 4; i < 8; i++) {
-            objStack.push(new ArrObj(i, i));
-        }
-		System.out.println(objStack.peek());
-		while (!objStack.isEmpty()) {
-		    System.out.println(objStack.pop());
-        }
+        System.out.println(recursion2(30));
+        System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
+        System.out.println(function2(30));
         System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
 
-		time = System.nanoTime();
-        QueueM<ArrObj> objQueue = new QueueM<>(objList);
-        for (int i = 1; i < 8; i++) {
-            objQueue.insert(new ArrObj(i, i));
-        }
-        System.out.println(objQueue.peek());
-        while (!objQueue.isEmpty()) {
-            System.out.println(objQueue.remove());
-        }
-        System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
 
-        time = System.nanoTime();
-        DequeM<ArrObj> objDeque = new DequeM<>(objList);
-        for (int i = 1; i < 8; i++) {
-            objDeque.addLast(new ArrObj(i, i));
-        }
-        System.out.println(objDeque.peekLast());
-        while (!objDeque.isEmpty()) {
-            System.out.println(objDeque.pollLast());
-        }
-        System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
-
-        time = System.nanoTime();
-        PriorityQueue<Integer> priorQueue = new PriorityQueue<>();
-        Random rand = new Random();
-        for (int i = 1; i < 8; i++) {
-            priorQueue.add(rand.nextInt(10));
-        }
-        System.out.println(priorQueue.peek());
-        while (!priorQueue.isEmpty()) {
-            System.out.println(priorQueue.poll());
-        }
-        System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
 
 	}
 
-}
-
-class ArrObj {
-
-	int a;
-	int b;
-
-	ArrObj (int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
-
-	@Override
-	public String toString() {
-		return "a: " + a + " b: " + b;
-	}
-}
-
-class StackM<T> {
-
-    LinkedList<T> stack;
-
-    StackM () {
-        stack = new LinkedList<>();
+	static int recursion1() {
+	    return recursion1();
     }
 
-    StackM (LinkedList<T> stack) {
-        this.stack = stack;
+    static int recursion2(int n) {
+        if (n == 0) {
+            return 1;
+        }
+	    return n*recursion2(n-1);
     }
 
-    void push (T Obj) {
-        stack.add(Obj);
+    static int function2(int n) {
+        int result = 1;
+	    for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+	    return result;
     }
 
-    T pop () {
-        T result = stack.getLast();
-        stack.remove(stack.size() - 1);
-        return result;
+    static int recursion3(int n) {
+        System.out.println("Вызов recursion3(" + n + ")");
+	    ++n;
+        if (n > 10) {
+            return 1;
+        }
+        return recursion3(n);
     }
 
-    T peek () {
-        return stack.getLast();
-    }
-
-    boolean isEmpty () {
-        return stack.size() == 0;
+    static int recursion4 (int[] arr, int low, int higt, int key) {
+	    int mid = (low + higt)/2;
+	    if (arr[mid] == key) {
+	        return mid;
+        }
+	    else if (arr[mid]<key) {
+	        return recursion4(arr, low + 1, higt, key);
+	    }
+	    else {
+            return recursion4(arr, low, higt - 1, key);
+        }
     }
 
 }
 
-class QueueM<T> {
-
-    LinkedList<T> queue;
-
-    QueueM() {
-        queue = new LinkedList<>();
-    }
-
-    QueueM(LinkedList<T> queue) {
-        this.queue = queue;
-    }
-
-    void insert(T Obj) {
-        queue.add(Obj);
-    }
-
-    T remove() {
-        T result = queue.getFirst();
-        queue.removeFirst();
-        return result;
-    }
-
-    T peek() {
-        return queue.getFirst();
-    }
-
-    boolean isEmpty() {
-        return queue.size() == 0;
-    }
-}
-
-class DequeM<T> {
-
-    LinkedList<T> deque;
-
-    DequeM() {
-        deque = new LinkedList<>();
-    }
-
-    DequeM(LinkedList<T> deque) {
-        this.deque = deque;
-    }
-
-    void addFirst(T Obj) {
-        deque.addFirst(Obj);
-    }
-
-    void addLast(T Obj) {
-        deque.addLast(Obj);
-    }
-
-    T pollFirst() {
-        T result = deque.getFirst();
-        deque.removeFirst();
-        return result;
-    }
-
-    T pollLast() {
-        T result = deque.getLast();
-        deque.removeLast();
-        return result;
-    }
-
-    T peekFirst() {
-        return deque.getFirst();
-    }
-
-    T peekLast() {
-        return deque.getLast();
-    }
-
-    boolean isEmpty() {
-        return deque.size() == 0;
-    }
-}
