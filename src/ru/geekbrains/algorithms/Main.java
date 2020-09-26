c12acb2f17f1e72c34aeec1c69909b2b0c61137d
Gulyaev Stepan <nlm@yandex.ru>
2020-09-26 19:35:01 +0300

    8 Lesson

--- src/ru/geekbrains/algorithms/Main.java
+++ src/ru/geekbrains/algorithms/Main.java
@@ -4,133 +4,123 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-
-        /*
-        * Пример древовидной структуры из жизни.
-        * Родословная.
-        * Любое генеологическое древо имеет
-        * древовидную структуру, даже название такоеже - древо.
-        * */
-
-        double time;
-        Graph graph = new Graph();
-        graph.addVertex('A');
-        graph.addVertex('B');
-        graph.addVertex('C');
-        graph.addVertex('D');
-        graph.addVertex('E');
-        graph.addVertex('F');
-
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
-
-        time = System.nanoTime();
-        graph.bfa();
-        System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
-
-        time = System.nanoTime();
-        graph.dfa();
-        System.out.println((System.nanoTime() - time) / 1_000_000_000 + "\n");
 
 
-    }
+    public static void main(String[] args) {
 
+        Item dataItem;
+        int size = 66;
 
-}
+        HashTable hTable = new HashTable(size);
 
-class Vertex {
+        for (int i = 0; i < 10; i++) {
+            dataItem = new Item(i);
+            hTable.insert(dataItem);
+        }
 
-    public char label;
-    public boolean wasVisited;
+        hTable.insert(new Item(123));
+        hTable.insert(new Item(1));
 
-    public Vertex (char label) {
-        this.label = label;
-        this.wasVisited = false;
+        hTable.display();
     }
 }
 
-class Graph {
+class Item {
 
-    private final int MAX = 32;
-    private Vertex[] vertexList;
-    private int[][] adjMat;
-    private int size;
-    private Stack stack;
+    private int data;
 
-    public Graph() {
-        stack = new Stack();
-        vertexList = new Vertex[MAX];
-        adjMat = new int[MAX][MAX];
-        size = 0;
-        for (int i = 0; i < MAX; i++) {
-            for (int j = 0; j < MAX; j++) {
-                adjMat[i][j] = 0;
+    public Item (int data) {
+        this.data = data;
+    }
+
+    public int getKey() {
+        return data;
+    }
+}
+
+class HashTable {
+
+    private Item[] hashArr;
+    private int arrSize;
+    private Item nonItem;
+
+    public HashTable(int size) {
+        this.arrSize = size;
+        hashArr = new Item[arrSize];
+        nonItem = new Item(-1);
+    }
+
+    public void display() {
+        for (int i = 0; i < arrSize; i++) {
+            if (hashArr[i] != null) {
+                System.out.println(hashArr[i].getKey());
+            } else {
+                System.out.println("***");
             }
         }
     }
 
-    public void addVertex(char label) {
-        vertexList[size++] = new Vertex(label);
+    public int hashFunc(int key) {
+        return key % arrSize;
     }
 
-    public void addEdge(int start, int end) {
-        adjMat[start][end] = 1;
-        adjMat[end][start] = 1;
+    public int hashFuncDouble(int key) {
+        return 5 - key % 5;
     }
 
-    public void displayVertex(int vertex) {
-        System.out.println(vertexList[vertex].label);
+    public void insert(Item item) {
+        int key = item.getKey();
+        int hashVal = hashFunc(key);
+        int stepSize = hashFuncDouble(key);
+        while (hashArr[hashVal] != null && hashArr[hashVal].getKey() != -1) {
+            hashVal += stepSize;
+            hashVal %= arrSize;
+        }
+        hashArr[hashVal] = item;
     }
 
-    public int getAdjUnvisitedVertex(int ver) {
-        for (int i = 0; i < size; i++) {
-            if (adjMat[ver][i] == 1 && vertexList[i].wasVisited == false) {
+    public Item delete(int key) {
+        int hashVal = hashFunc(key);
+        int stepSize = hashFuncDouble(key);
+        while (hashArr[hashVal] != null) {
+            if (hashArr[hashVal].getKey() == key) {
+                Item temp = hashArr[hashVal];
+                hashArr[hashVal] = nonItem;
+                return temp;
+            }
+            hashVal += stepSize;
+            hashVal %= arrSize;
+        }
+        return null;
+    }
+
+    public Item find(int key) {
+        int hashVal = hashFunc(key);
+        int stepSize = hashFuncDouble(key);
+        while (hashArr[hashVal] != null) {
+            if (hashArr[hashVal].getKey() == key) {
+                return hashArr[hashVal];
+            }
+            hashVal += stepSize;
+            hashVal %= arrSize;
+        }
+        return null;
+    }
+
+    private int getPrime(int min) {
+        for (int i = min; true; i++) {
+            if (isPrime(i)) {
                 return i;
             }
         }
-        return -1;
     }
 
-    public void bfa() {
-        Queue<Integer> queue = new LinkedList<>();
-        vertexList[0].wasVisited = true;
-        displayVertex(0);
-        queue.add(0);
-        int v2;
-        while (!queue.isEmpty()){
-            int v1 = queue.remove();
-            while ((v2 = getAdjUnvisitedVertex(v1)) != -1) {
-                vertexList[v2].wasVisited = true;
-                displayVertex(v2);
-                queue.add(v2);
+    private boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-        for (int i = 0; i < size; i++) {
-            vertexList[i].wasVisited = false;
-        }
-    }
-
-    public void dfa() {
-        vertexList[0].wasVisited = true;
-        displayVertex(0);
-        stack.push(0);
-        while (!stack.isEmpty()){
-            int v = getAdjUnvisitedVertex((Integer) stack.peek());
-            if (v == -1) {
-                stack.pop();
-            } else {
-                vertexList[v].wasVisited = true;
-                displayVertex(v);
-                stack.push(v);
-            }
-        }
-        for (int i = 0; i < size; i++) {
-            vertexList[i].wasVisited = false;
-        }
+        return true;
     }
 }
